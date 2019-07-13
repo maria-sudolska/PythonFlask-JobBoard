8b365ba8e66105868c4b83989805ecca90512075
Maria Sudolska <m.sudolska@gmail.com>
2019-07-13 20:33:51 +0200

    Completed the module

--- jobs/app.py
+++ jobs/app.py
@@ -1,7 +1,36 @@
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask, render_template, g
+
+# Path to database
+PATH = 'db/jobs.sqlite'
 
 # Create an instance of the Flask class called app
 app = Flask(__name__)
+
+def open_connection():
+    connection = getattr(g, '_connection', None)
+    if connection == None:
+        connection = g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
+    return connection
+
+def execute_sql(sql, values=(), commit=False, single=False):
+    connection = open_connection()
+    cursor = connection.execute(sql, values)
+    if commit == True:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
+
+    cursor.close()
+    return results
+
+# Ensure that close_connection function is called when the app_context is destroyed
+@app.teardown_appcontext
+def close_connection(exception):
+    connection = getattr(g, '_connection', None)
+    if connection is not None:
+        connection.close()
 
 # Route decorators
 @app.route('/')
